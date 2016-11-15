966c97da2ec1e7bd52cc616bea088cc50e4474b2
Ricardo Hernandez <a.ricardohg@gmail.com>
2016-11-15 17:54:29 -0500

    update to swift 3

--- customlist.playground/Contents.swift
+++ customlist.playground/Contents.swift
@@ -3,68 +3,56 @@
 import UIKit
 
 enum List<Element> {
-    case End
-    indirect case Node(Element, next:List<Element>)
+    case end
+    indirect case node(Element, next:List<Element>)
 }
 
 extension List {
-    func cons(x:Element) -> List {
-        return .Node(x, next:self)
+    func cons(_ x:Element) -> List {
+        return .node(x, next:self)
     }
 }
 
-protocol StackType {
+let list = List<Int>.end.cons(3).cons(2).cons(1)
+
+extension List: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: Element...) {
+        self = elements.reversed().reduce(.end) { partialList, element in
+            partialList.cons(element)
+        }
+    }
+}
+
+protocol Stack {
     associatedtype Element
-    mutating func push(x:Element)
+    mutating func push(_ :Element)
     mutating func pop() -> Element?
 }
 
-extension Array: StackType {
-    mutating func push(x: Element) {
-        append(x)
-    }
-    
-    mutating func pop() -> Element? {
-        return popLast()
-    }
+extension Array: Stack {
+    mutating func push(_ x: Element) { append(x) }
+    mutating func pop() -> Element? { return popLast() }
 }
 
-extension List: StackType {
-    mutating func push(x: Element) {
+extension List: Stack {
+    mutating func push(_ x: Element) {
         self = self.cons(x)
     }
     
     mutating func pop() -> Element? {
         switch self {
-        case .End: return nil
-        case let .Node(x, next:xs):
+        case .end: return nil
+        case let .node(x, next:xs):
             self = xs
             return x
         }
     }
 }
 
-extension List: SequenceType {
-    func generate() -> AnyGenerator<Element> {
-        var current = self
-        return AnyGenerator {
-            current.pop()
-        }
+extension List: IteratorProtocol, Sequence {
+    mutating func next() -> Element? {
+        return pop()
     }
 }
-
-extension List: ArrayLiteralConvertible {
-    init(arrayLiteral elements: Element...) {
-        self = elements.reverse().reduce(.End){ l, x in l.cons(x) }
-    }
-}
-let l = List<Int>.End.cons(1).cons(2).cons(3)
-let newList :List = [1, 2, 3]
-
-for x in newList {
-    print("\(x)")
-}
-
-let f = l.filter{ $0 > 2 }
 
 
